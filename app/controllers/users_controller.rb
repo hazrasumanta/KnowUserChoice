adf93995a64f7731e20ee24ee4e170a32337fe39
Sumanta Hazra <hazrasumanta@gmail.com>
2017-11-14 00:45:36 +0530

    fix some issue on user topic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,21 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
   #before_action :check_token, only: [:topics]
   wrap_parameters :user, include: [:name, :password,:phone,:email, :password_confirmation]
-  # GET /users
+
+
+  #getting user topics
+  def topics
+    if check_token()
+      all_user_topic = @user_token.topic_of_interests
+      all_user_topic = all_user_topic.to_json(:root => false,
+             :only => [:id,:topic])
+      render json: all_user_topic
+    else
+      render json:  {status: :false, message: :user_authentication_failed}
+    end
+  end
+
+  # POST /users
   
   #add signup user
   def signup
@@ -16,9 +30,8 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:name, :phone, :email, :password, :password_confirmation, :last_sign_in_at)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:name, :phone, :email, :password, :password_confirmation, :last_sign_in_at)
+  end
 end
