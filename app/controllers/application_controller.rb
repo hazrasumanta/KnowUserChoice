154946b0646058a3ad426ebe583169c168f018b2
Sumanta Hazra <hazrasumanta@gmail.com>
2017-11-13 13:36:51 +0530

    added user and its controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- db/migrate/20171113074715_create_users.rb
+++ db/migrate/20171113074715_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :phone
       t.string :email
-      t.digest :passworld
+      t.string :password_digest
       t.datetime :last_sign_in_at
 
       t.timestamps
