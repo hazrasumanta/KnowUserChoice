a6fdc2b42c1131cc170906e75be5cb0a17419aae
Sumanta Hazra <hazrasumanta@gmail.com>
2017-11-13 16:13:17 +0530

    add signup action to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-
+  wrap_parameters :user, include: [:name, :password,:phone,:email, :password_confirmation]
   # GET /users
   def index
     @users = User.all
@@ -22,6 +22,17 @@ class UsersController < ApplicationController
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  def signup
+     @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+
   end
 
   # PATCH/PUT /users/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destroy'
+
+  resources :users do
+  	collection do
+  		post :signup
+  	end
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
